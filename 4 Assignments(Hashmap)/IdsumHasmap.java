34ea85fcd5a6e207a18513c9fa05e2fd8a08da22
Muhammad Shamail Nadir <mshamailnadir7861@gmail.com>
2026-05-10 00:52:43 +0500

    Create IdsumHasmap.java

---
+++ 4 Assignments(Hashmap)/IdsumHasmap.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class IdsumHasmap {
+    public static void main(String[] args) {
+        File f1 = new File("F:\CS-351 Programing Fundamentals\sale.txt");
+        Scanner inp;
+        String dr;
+        HashMap<String, Double> salesMap = new HashMap<String, Double>();
+        try {
+            inp = new Scanner(f1);
+            dr = inp.nextLine(); 
+            String[] parts;
+            String id;                   
+            double amt, uPrice, qty;
+
+            while (inp.hasNextLine()) {
+                dr = inp.nextLine();
+                if (dr.trim().length() == 0) continue;
+
+                parts    = dr.split("\t");
+                id = parts[5];                      
+   
+                uPrice   = Double.parseDouble(parts[2]);  
+                qty      = Double.parseDouble(parts[3]);  
+                amt      = uPrice * qty;
+
+                if (salesMap.containsKey(id)) {
+                    salesMap.put(id, salesMap.get(id) + amt);
+                } else {
+                    salesMap.put(id, amt);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("===== REP ID-WISE TOTAL SALES (HASHMAP) =====");
+            for (Map.Entry<String, Double> entry : salesMap.entrySet()) {
+                System.out.println(entry.getKey() + " ---> " + entry.getValue());
+            }
+        }
+    }
+}
